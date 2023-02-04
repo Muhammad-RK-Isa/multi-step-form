17c2325b8527d3d06606c33614706e904442d626
Muhammad-RK-Isa <muhammad-rk-isa@outlook.com>
2023-02-05 03:05:52 +0600

    Readme.md edited

--- theme.js
+++
@@ -1,8 +0,0 @@
-const root = document.querySelector(':root');
-const rootStyles = getComputedStyle(root);
-const themeColor = rootStyles.getPropertyValue('--color-white');
-const themeChanger = () => {
-    root.style.setProperty('--color-white', 'black');
-};
-// themeChanger();
-// console.log(themeColor);
